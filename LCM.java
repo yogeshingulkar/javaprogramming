756920420e4387a13d1e8e8bd8d94ce7e6dda52a
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2025-09-24 21:51:02 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class LCM{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a First Number : ");
+		int n1 = sc.nextInt();
+
+		System.out.println("Enter a Second Number : ");
+		int n2 = sc.nextInt();
+
+		int largest = n1 > n2 ? n1 : n2 ;
+
+		for (int i = 1; ;i++){
+			if((largest*i)%n1==0 && (largest*i)%n2==0){
+					System.out.println("The LCM of "+n1+" and "+n2+" is "+(largest*i));
+					break ;
+			}
+		}
+	}
+}
